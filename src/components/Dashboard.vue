b517e99388a51e13ddc9ce55dab3ff40565402a9
Ikechi Michael <mykehell123@gmail.com>
2018-07-09 18:39:10 +0100

    feat: move vehicles state to vuex

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -48,31 +48,14 @@
         return {
 
         }
+      },
+      tableData () {
+        return this.$store.state.vehicles
       }
     },
     data () {
       return {
-        columns: [ 'id', 'owner', 'reg_no' ],
-        tableData: [
-          {
-            id: 14,
-            owner: 'Joseph Cobhams',
-            reg_no: '12345',
-            slack_id: 'U8GSNUN8Z'
-          },
-          {
-            id: 17,
-            owner: 'Abdulqahhar Aminujatto',
-            reg_no: '098765',
-            slack_id: 'U6HKDGE6L'
-          },
-          {
-            id: 18,
-            owner: 'Jolomi Otumara',
-            reg_no: 'fst-776-cx',
-            slack_id: 'U740UK4SG'
-          }
-        ]
+        columns: [ 'id', 'owner', 'reg_no' ]
       }
     }
   }
--- src/store/store.state.js
+++ src/store/store.state.js
@@ -1,3 +1,22 @@
 export default {
-
+  vehicles: [
+    {
+      id: 14,
+      owner: 'Joseph Cobhams',
+      reg_no: '12345',
+      slack_id: 'U8GSNUN8Z'
+    },
+    {
+      id: 17,
+      owner: 'Abdulqahhar Aminujatto',
+      reg_no: '098765',
+      slack_id: 'U6HKDGE6L'
+    },
+    {
+      id: 18,
+      owner: 'Jolomi Otumara',
+      reg_no: 'fst-776-cx',
+      slack_id: 'U740UK4SG'
+    }
+  ]
 }
