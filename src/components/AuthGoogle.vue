2b829fe921a29d588b75aba992624b256de3b4b5
Ikechi Michael <mykehell123@gmail.com>
2018-07-09 22:20:17 +0100

    feat: create AuthGoogle component

---
+++ src/components/AuthGoogle.vue
@@ -0,0 +1,49 @@
+<template>
+  <Login :error-text="errorText"></Login>
+</template>
+
+<script>
+  import Login from './Login'
+  import queryString from 'query-string'
+  import jwt from 'jsonwebtoken'
+  import { LOGIN_USER } from '../store/store.constants'
+
+  export default {
+    props: {
+
+    },
+    computed: {
+
+    },
+    data () {
+      return {
+        errorText: null
+      }
+    },
+    components: {
+      Login
+    },
+    methods: {
+
+    },
+    mounted () {
+      const queries = queryString.parse(location.search)
+      if (queries.token) {
+        try {
+          const user = jwt.decode(queries.token)
+          if (!user) throw new Error('Could not parse token')
+          this.$store.commit(LOGIN_USER, { ...user.UserInfo, ...queries })
+
+        }
+        catch (ex) {
+          this.errorText = ex.message || 'An error occurred'
+        }
+      }
+      console.log(queries)
+    }
+  }
+</script>
+
+<style>
+
+</style>
